397cd0cf87dbb74a47c6584c88e0a79f50d37b81
Bachi007 <chitturi.bhaskar007@gmail.com>
2025-05-01 10:31:02 +0530

    first commit

---
+++ src/test/java/com/digtalrebels/limit_service/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digtalrebels.limit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
